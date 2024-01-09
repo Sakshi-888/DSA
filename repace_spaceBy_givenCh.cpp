6dce970d17743df63625b28eb8a5aa2fa42be554
Sakshi Sharma <sharmasakshi8546@gmail.com>
2024-01-09 16:37:16 +0530

    optimal approach added

--- repace_spaceBy_givenCh.cpp
+++ repace_spaceBy_givenCh.cpp
@@ -1,6 +1,9 @@
 /*
 You have been given a string 'STR' of words. You need to replace all the spaces between words with “@40”.
 */
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
 #include<bits\stdc++.h>
 using namespace std;
 
@@ -24,10 +27,60 @@ string replaceSpaces(string &str){
 	return temp;
 }
 
+
+// Without using extra space (in-place manipulation)
+/*
+	Time Complexity : O(N)
+	Space Complexity : O(1)
+
+	Where 'N' is size of string of words.
+*/
+
+string replaceSpaces(string &str){
+	int spaceCount = 0;
+	
+	// Count Number of Spaces.
+	for(int i = 0; i < str.size(); i++){
+		if(str[i] == ' '){
+			spaceCount = spaceCount + 1;
+		}
+	}
+
+	// Store thr current length of the string in a variable.
+	int len = str.size();
+	
+	// Find the new length.
+	int newLength;
+	newLength = len + (spaceCount * 2);
+	
+	// Resize the current string.
+	str.resize(newLength);
+	
+	// Index to end of string.
+	int index = newLength - 1;
+	
+	// Fill string from end.
+	for(int k = len - 1 ; k >= 0; k--){
+		if(str[k] == ' '){
+			str[index] = '0';
+            str[index - 1] = '4';
+            str[index - 2] = '@';
+            index = index - 3;
+		}
+		else{
+			str[index] = str[k];
+			index = index - 1;
+		}
+	}	
+	// Return string.
+	return str;	
+}
+
 int main()
 {
     string s;
     getline(cin,s);
     cout<<replaceSpaces(s);
 }
+
 
